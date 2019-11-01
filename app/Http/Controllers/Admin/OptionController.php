98a18826603fd1ded34b865de7e62f5493a1fca5
UltrmanOrbr <1216281190@qq.com>
2019-11-01 11:04:29 +0800

    z

---
+++ app/Http/Controllers/Admin/OptionController.php
@@ -0,0 +1,124 @@
+<?php
+/**
+created by z
+ * time 2019-10-31 14:02:03
+ */
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+use App\Models\Option;
+class OptionController extends Controller
+{
+    /**
+    首页
+     */
+    public function index(){
+        $data=Option::paginate();
+        foreach ($data as $key =>$value){
+            $data[$key]['creattime']=date("Y-m-d H:i:s",$value["creattime"]);
+        }
+        return view('options.list',['pager'=>$data]);
+    }
+
+
+    /**
+    编辑页
+     */
+    public function edit($id=0){
+        $info = $id?Option::find($id):[];
+        return view('options.edit',['id'=>$id,'info'=>$info]);
+    }
+
+    /**
+     * 用户增加保存
+     */
+    public function store(StoreRequest $request){
+        $id =$request->input('id');
+        $data=$request->all();
+        unset($data['_token']);
+        $res=$this->add_unique($data['key'],$data['value']);
+        if(!$res){
+            $data['creattime']=time();
+            $insert=Option::insert($data);
+            if($insert){
+                $key=$data['key'].'_'.$data['value'];
+                $value=$data['content'];
+                Redis::set($key,$value);
+                return ['msg'=>'添加成功！','status'=>1];
+            }else{
+                return ['msg'=>'添加失败！'];
+            }
+
+        }else{
+            return ['msg'=>'配置已存在！'];
+        }
+
+
+    }
+
+    /**
+     * 保存
+     */
+    public function update(StoreRequest $request){
+        $id =$request->input('id');
+        $data=$request->all();
+        unset($data['_token']);
+        unset($data['id']);
+        $info = Option::find($id);
+        $key=$info['key'].'_'.$info['value'];
+        Redis::del($key);
+        $res=$this->edit_unique($id,$data['key'],$data['value']);
+        if(!$res){
+            $update=Option::where('id',$id)->update($data);
+            if($update!==false){
+                $k=$data['key'].'_'.$data['value'];
+                $v=$data['content'];
+                Redis::set($k,$v);
+                return ['msg'=>'修改成功！','status'=>1];
+            }else{
+                return ['msg'=>'修改失败！'];
+            }
+        }else{
+            return ['msg'=>'配置已存在！'];
+        }
+
+    }
+
+    /**
+     * 删除
+     */
+    public function destroy($id){
+        $res = Option::where('id', '=', $id)->delete();
+        if($res){
+            return ['msg'=>'删除成功！','status'=>1];
+        }else{
+            return ['msg'=>'删除失败！'];
+        }
+    }
+
+    /**
+     * 添加判断存在
+     */
+    private function add_unique($key,$value){
+        $res=Option::where(array('key'=>$key,'value'=>$value))->exists();
+        if($res){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * 编辑判断存在
+     */
+    private function edit_unique($id,$key,$value){
+        $res=Option::where(array('key'=>$key,'value'=>$value))->whereNotIn('id',[$id])->exists();
+        if($res){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
---
+++ resources/views/options/list.blade.php
@@ -0,0 +1,84 @@
+@section('title', '配置列表')
+@section('header')
+    <div class="layui-inline">
+        <a class="layui-btn layui-btn-small layui-btn-normal" data-desc="添加配置" onclick="add()"><i class="layui-icon">&#xe654;</i></a>
+        <button class="layui-btn layui-btn-small layui-btn-warm freshBtn"><i class="layui-icon">&#x1002;</i></button>
+    </div>
+@endsection
+@section('table')
+    <table class="layui-table" lay-even lay-skin="nob">
+        <colgroup>
+            <col class="hidden-xs" width="50">
+            <col class="hidden-xs" width="200">
+            <col class="hidden-xs" width="150">
+            <col>
+            <col class="hidden-xs" width="150">
+            <col width="200">
+        </colgroup>
+        <thead>
+        <tr>
+            <th class="hidden-xs">ID</th>
+            <th class="hidden-xs">key</th>
+            <th class="hidden-xs">value</th>
+            <th>内容</th>
+            <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($pager as $info)
+            <tr>
+                <td class="hidden-xs">{{$info['id']}}</td>
+                <td class="hidden-xs">{{$info['key']}}</td>
+                <td class="hidden-xs">{{$info['value']}}</td>
+                <td>{{$info['content']}}</td>
+                <td>
+                    <div class="layui-inline">
+                        <button class="layui-btn layui-btn-small layui-btn-normal edit-btn" data-id="{{$info['id']}}" data-desc="修改配置" data-url="{{url('/admin/options/'. $info['id'] .'/edit')}}"><i class="layui-icon">&#xe642;</i></button>
+                        <button class="layui-btn layui-btn-small layui-btn-danger del-btn" data-id="{{$info['id']}}" data-url="{{url('/admin/options/'.$info['id'])}}"><i class="layui-icon">&#xe640;</i></button>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+        @if(!$pager[0])
+            <tr><td colspan="6" style="text-align: center;color: orangered;">暂无数据</td></tr>
+        @endif
+        </tbody>
+    </table>
+    <div class="page-wrap">
+        {{$pager->render()}}
+    </div>
+@endsection
+@section('js')
+    <script>
+
+        layui.use(['form', 'jquery','laydate', 'layer'], function() {
+            var form = layui.form(),
+                $ = layui.jquery,
+                laydate = layui.laydate,
+                layer = layui.layer
+            ;
+            laydate({istoday: true});
+            form.render();
+            form.on('submit(formDemo)', function(data) {
+                console.log(data);
+            });
+            //layer.msg(layui.v);
+        });
+
+        function add() {
+            layer.open({
+                type: 2,
+                title: '添加配置',
+                closeBtn: 1,
+                area: ['400px','400px'],
+                shadeClose: true, //点击遮罩关闭
+                resize:false,
+                content: ['/admin/options/0/edit','no'],
+                end:function(){
+
+                }
+            });
+        }
+    </script>
+@endsection
+@extends('common.list')
