9e87b66c60c61c3ae1e571eb49d30aa297b2f814
UltrmanOrbr <1216281190@qq.com>
2019-11-05 17:27:04 +0800

    z

--- config/database.php
+++ config/database.php
@@ -131,7 +131,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
-            'prefix' => "test_",
+            'prefix' => "admin_",
         ],
 
     ],
