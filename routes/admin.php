103e833463966a562368eecaaa2d34124ee11fbb
UltrmanOrbr <1216281190@qq.com>
2019-11-16 09:02:52 +0800

    z

--- routes/admin.php
+++ routes/admin.php
@@ -67,66 +67,66 @@ Route::group(['namespace' => "Admin",'middleware' => ['auth', 'permission']], fu
     Route::resource('/busdrawdone',     'BusdrawdoneController');//通过列表
     Route::resource('/busdrawreject',   'BusdrawrejectController');
     Route::resource('/agent',           'AgentController');//代理商
-    Route::get('/agent/agentbankinfo/{id}', 'AgentController@agentbankinfo');
-    Route::get('/agent/editpwd/{id}',   'AgentController@editpwd');
+    Route::get('/agent/agentbankinfo/{id}', 'AgentController@agentbankinfo');//代理银行信息
+    Route::get('/agent/editpwd/{id}',   'AgentController@editpwd');//代理登录密码
     Route::post('/changepwd',           'AgentController@changepwd');
-    Route::get('/agent/editpayword/{id}','AgentController@editpayword');
+    Route::get('/agent/editpayword/{id}','AgentController@editpayword');//代理支付密码
     Route::post('/changepayword',       'AgentController@changepayword');
     Route::post('/agentUpdate',         'AgentController@update');
-    Route::post('/agent_switch',        'AgentController@agent_switch');
-    Route::post('/agent_islogin',       'AgentController@agent_islogin');
-    Route::resource('/agentdrawnone',   'AgentdrawnoneController');
+    Route::post('/agent_switch',        'AgentController@agent_switch');//代理状态
+    Route::post('/agent_islogin',       'AgentController@agent_islogin');//代理登陆
+    Route::resource('/agentdrawnone',   'AgentdrawnoneController');//代理提现
     Route::post('/agentdrawnone/pass',  'AgentdrawnoneController@pass');
     Route::post('/agentdrawnone/reject','AgentdrawnoneController@reject');
     Route::resource('/agentdrawdone',   'AgentdrawdoneController');
     Route::resource('/agentdrawreject', 'AgentdrawrejectController');
-    Route::resource('/codeuser',        'CodeUserController');
-    Route::post('/codeuserUpdate',      'CodeUserController@update');
-    Route::post('/codeuser_isover',     'CodeUserController@codeuser_isover');
+    Route::resource('/codeuser',        'CodeUserController');//码商列表
+    Route::post('/codeuserUpdate',      'CodeUserController@update');//码商编辑
+    Route::post('/codeuser_isover',     'CodeUserController@codeuser_isover');//状态开关
 
-    Route::get('/codeuser/addqr/{id}',  'CodeUserController@addqr');
+    Route::get('/codeuser/addqr/{id}',  'CodeUserController@addqr');//码商增加二维码数量
     Route::post('/codeaddqr',           'CodeUserController@codeaddqr');
-    Route::get('/codeuser/tomsg/{id}',  'CodeUserController@tomsg');
+    Route::get('/codeuser/tomsg/{id}',  'CodeUserController@tomsg');//码商通知
     Route::post('/codeputmsg',          'CodeUserController@codeputmsg');
-    Route::get('/codeuser/ownfee/{id}',  'CodeUserController@ownfee');
+    Route::get('/codeuser/ownfee/{id}',  'CodeUserController@ownfee');//码商费率
     Route::post('/codeuserfee',          'CodeUserController@codeuserfee');
-    Route::get('/codeuser/logpwd/{id}',  'CodeUserController@logpwd');
+    Route::get('/codeuser/logpwd/{id}',  'CodeUserController@logpwd');//码商登录密码
     Route::post('/codenewpwd',          'CodeUserController@codenewpwd');
-    Route::get('/codeuser/secondpwd/{id}', 'CodeUserController@secondpwd');
+    Route::get('/codeuser/secondpwd/{id}', 'CodeUserController@secondpwd');//码商二级密码/已弃用
     Route::post('/codenewTwopwd',         'CodeUserController@codenewTwopwd');
-    Route::get('/codeuser/zfpwd/{id}',  'CodeUserController@zfpwd');
+    Route::get('/codeuser/zfpwd/{id}',  'CodeUserController@zfpwd');//码商支付密码
     Route::post('/codenewpaypwd',         'CodeUserController@codenewpaypwd');
 
-    Route::get('/codeownbill/own/{id}',  'CodeownbillController@own');
+    Route::get('/codeownbill/own/{id}',  'CodeownbillController@own');//码商个人流水
     Route::resource('/codeownbill',      'CodeownbillController');
 
 
-    Route::resource('/coderakemoney',   'CoderakemoneyController');
+    Route::resource('/coderakemoney',   'CoderakemoneyController');//码商激活佣金
     Route::post('/coderakemoneyUpdate', 'CoderakemoneyController@update');
-    Route::resource('/codedrawnone',    'CodedrawnoneController');
+    Route::resource('/codedrawnone',    'CodedrawnoneController');//码商提现
     Route::post('/codedrawnone/pass',   'CodedrawnoneController@pass');
     Route::post('/codedrawnone/reject', 'CodedrawnoneController@reject');
     Route::resource('/codedrawdone',    'CodedrawdoneController');
     Route::resource('/codedrawreject',  'CodedrawrejectController');
-    Route::resource('/recharge',        'RechargeController');
+    Route::resource('/recharge',        'RechargeController');//充值信息
     Route::post('/recharge/enable',     'RechargeController@enable');
-    Route::resource('/rechargelist',    'RechargelistController');
+    Route::resource('/rechargelist',    'RechargelistController');//充值列表管理审核
     Route::post('/rechargelist/enable', 'RechargelistController@enable');
-    Route::resource('/billflow',        'BillflowController');
-    Route::resource('/order',           'OrderController');
-    Route::post('/order/budan',         'OrderController@budan');
-    Route::post('/order/csbudan',       'OrderController@csbudan');
-    Route::post('/order/sfpushfirst',   'OrderController@sfpushfirst');
-    Route::resource('/buscount',        'BuscountController');
-    Route::resource('/busbill',         'BusbillController');
-    Route::resource('/agentcount',      'AgentcountController');
-    Route::resource('/agentbill',       'AgentbillController');
-    Route::resource('/codecount',       'CodecountController');
-    Route::resource('/codebill',        'BillflowController');
-    Route::resource('/busbank',         'BusbankController');
-    Route::resource('/agentbank',       'AgentbankController');
-    Route::resource('/qrcode',          'QrcodeController');
-    Route::resource('/datacount',       'DatacountController');
+    Route::resource('/billflow',        'BillflowController');//码商流水
+    Route::resource('/order',           'OrderController');//订单列表
+    Route::post('/order/budan',         'OrderController@budan');//订单补单
+    Route::post('/order/csbudan',       'OrderController@csbudan');//订单超时补单
+    Route::post('/order/sfpushfirst',   'OrderController@sfpushfirst');//订单手动补单
+    Route::resource('/buscount',        'BuscountController');//商户账单
+    Route::resource('/busbill',         'BusbillController');//商户流水
+    Route::resource('/agentcount',      'AgentcountController');//代理账单
+    Route::resource('/agentbill',       'AgentbillController');//代理流水
+    Route::resource('/codecount',       'CodecountController');//码商账单
+    Route::resource('/codebill',        'BillflowController');//码商流水
+    Route::resource('/busbank',         'BusbankController');//商户银行
+    Route::resource('/agentbank',       'AgentbankController');//代理银行
+    Route::resource('/qrcode',          'QrcodeController');//码商-二维码列表
+    Route::resource('/datacount',       'DatacountController');//平台数据统计
 });
 
 Route::get('/phpinfo',function (Request $request){
