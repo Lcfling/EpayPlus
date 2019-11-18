09fea976078c582800309cee3e36f4e7d101e332
UltrmanOrbr <1216281190@qq.com>
2019-11-18 08:40:56 +0800

    z

--- app/Http/Controllers/Admin/BillflowController.php
+++ app/Http/Controllers/Admin/BillflowController.php
@@ -46,7 +46,7 @@ class BillflowController extends Controller
             }
             exportExcel($head,$excel,'码商流水'.date('YmdHis',time()),'',true);
         }else{
-            $data=$sql->paginate(10)->appends($request->all());
+            $data=$sql->orderBy('creatime','desc')->paginate(10)->appends($request->all());
             foreach ($data as $key=>$value){
                 $data[$key]['creatime']=date("Y-m-d H:i:s",$value["creatime"]);
             }
